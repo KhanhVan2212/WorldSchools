9944637d35866c319309329d34ce90d380829ad0
nhatnvph <nhatngo20102002@gmail.com>
2024-06-30 12:03:56 +0700

    update search

--- Client/src/components/Admin/AdminProduct/CreateProduct.jsx
+++ Client/src/components/Admin/AdminProduct/CreateProduct.jsx
@@ -6,12 +6,12 @@ import { ProductCreate } from "../../../services/product";
 
 const CreateProduct = () => {
   const queryClient = useQueryClient();
-  const {register , handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
-  const {mutate} = useMutation({
-    mutationFn: async(product) => {
-     const data = await ProductCreate(product);
-     return data;
+  const { mutate } = useMutation({
+    mutationFn: async (product) => {
+      const data = await ProductCreate(product);
+      return data;
     },
     onSuccess: () => {
       queryClient.invalidateQueries({
@@ -19,69 +19,73 @@ const CreateProduct = () => {
       });
     },
     onError: (error) => {
-      console.error('Failed to add product:', error);
-   },
+      console.error("Failed to add product:", error);
+    },
   });
   const onSubmit = async (product) => {
-    mutate(product)
+    mutate(product);
     navigate("/admin/products");
-};
+  };
   return (
-      <>
-      <form onSubmit={handleSubmit(onSubmit)}
-            className="space-y-6"
-            id="add-form">
-      <div className="min-h-screen p-6 z-40 flex items-center justify-center ml-12">
-      <div className="container max-w-screen-lg mx-auto">
-        <div>
-          <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-              <div className="text-gray-600">
-                <p className="font-medium text-lg">Create Product</p>
-                <p>Please fill out all the fields.</p>
-              </div>
-              
-              <div className="lg:col-span-2">
-                <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                <div className="md:col-span-5">
-                    <label htmlFor="email">Title</label>
-                    <input
-                      type="text"
-                      {...register("title")}
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                    />
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-6"
+        id="add-form"
+      >
+        <div className="min-h-screen p-6 z-40 flex items-center justify-center ml-12">
+          <div className="container max-w-screen-lg mx-auto">
+            <div>
+              <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+                <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+                  <div className="text-gray-600">
+                    <p className="font-medium text-lg">Create Product</p>
+                    <p>Please fill out all the fields.</p>
                   </div>
 
-                  <div className="md:col-span-5">
-                    <label htmlFor="email">Poster</label>
-                    <input
-                      type="text"
-                      {...register("poster")}
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                    />
-                  </div>
-                  <div className="md:col-span-5">
-                    <label htmlFor="email">Image</label>
-                    <input
-                      type="text"
-                      {...register("image")}
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                    />
-                  </div>
-                  <div className="md:col-span-5">
-                    <label htmlFor="email">Description</label>
-                    <textarea 
-                      type="text"
-                      {...register("description")}
-                      className="h-28 border mt-1 rounded px-4 w-full bg-gray-50"
-                    />
-                  </div>
+                  <div className="lg:col-span-2">
+                    <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                      <div className="md:col-span-5">
+                        <label htmlFor="email">Title</label>
+                        <input
+                          type="text"
+                          {...register("title")}
+                          className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        />
+                      </div>
 
-                  <div className="md:col-span-5 text-right">
-                    <div className="inline-flex items-end">
-                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Submit
-                      </button>
+                      <div className="md:col-span-5">
+                        <label htmlFor="email">Poster</label>
+                        <input
+                          type="text"
+                          {...register("poster")}
+                          className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        />
+                      </div>
+                      <div className="md:col-span-5">
+                        <label htmlFor="email">Image</label>
+                        <input
+                          type="text"
+                          {...register("image")}
+                          className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                        />
+                      </div>
+                      <div className="md:col-span-5">
+                        <label htmlFor="email">Description</label>
+                        <textarea
+                          type="text"
+                          {...register("description")}
+                          className="h-28 border mt-1 rounded px-4 w-full bg-gray-50"
+                        />
+                      </div>
+
+                      <div className="md:col-span-5 text-right">
+                        <div className="inline-flex items-end">
+                          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Submit
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -89,10 +93,8 @@ const CreateProduct = () => {
             </div>
           </div>
         </div>
-      </div>
-    </div>
       </form>
-      </> 
+    </>
   );
 };
 
